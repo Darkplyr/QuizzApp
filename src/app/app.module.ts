7fe0719a0387fb0424fd73afc64f5f0ab0a9aa9e
Darkplyr <74112342+Darkplyr@users.noreply.github.com>
2021-09-11 19:56:52 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { Cmp1Component } from './cmp1/cmp1.component';
+import { Cmp2Component } from './cmp2/cmp2.component';
+import { Cmp3Component } from './cmp3/cmp3.component';
+
+const routes: Routes = [
+  { path: 'c1', component: Cmp1Component },
+  { path: 'c2', component: Cmp2Component },
+  { path: 'c3', component: Cmp3Component }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
