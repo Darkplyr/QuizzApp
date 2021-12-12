16d81920843cfec51ed0dcdb6205e8db849bfcd3
Darkplyr <74112342+Darkplyr@users.noreply.github.com>
2021-12-12 19:18:27 +0200

    leaderboard fix

--- src/app/quiz-api-service.service.ts
+++ src/app/quiz-api-service.service.ts
@@ -4,11 +4,10 @@ import { HttpClient } from "@angular/common/http"
 @Injectable({
   providedIn: 'root'
 })
-export class QuizApiServiceService {
 
+export class QuizApiServiceService {
   private apiUrl : string = "";
   public apiToken : string = "";
-
   categories : any = [];
   quizzes : any = [];
   scores : any = [];
@@ -16,14 +15,13 @@ export class QuizApiServiceService {
   currentIndex = 0;
   currentScore = 0;
   
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { 
-  }
-
+  // Call and get the questions from the API
   getQuestions() : void 
   {
     this.generateApiUrl();
-    this.quizzes=[];
+    this.quizzes = [];
     this.http.get(this.apiUrl)
     .subscribe((data : any) => {
       for (let i = 0; i < data.results.length; i++) {
@@ -35,10 +33,10 @@ export class QuizApiServiceService {
           type : decodeURIComponent(data.results[i].type),
           question_category: decodeURIComponent(data.results[i].category)
         }
-        if(q.choices.length == 1) {
+        if (q.choices.length == 1) {
           q.choices = ["True", "False"]
         }
-        else{
+        else {
           q.choices = this.decodeArray(q.choices);
           q.choices.splice(Math.floor(Math.random() * 4), 0, q.answer);
         }
@@ -47,8 +45,9 @@ export class QuizApiServiceService {
     })
   }
 
+  // Get the question categories 
   getCategories() : void {
-    this.categories=[];
+    this.categories = [];
     this.http.get("https://opentdb.com/api_category.php")
     .subscribe((data : any) => {
       let c = {
@@ -66,6 +65,7 @@ export class QuizApiServiceService {
     })
   }
 
+  // Decodes the special characters in an array of strings
   decodeArray(array : any) : any {
     for (let i = 0; i < array.length; i++) {
       array[i] = decodeURIComponent(array[i]);
@@ -73,11 +73,13 @@ export class QuizApiServiceService {
     return array;
   }
 
+  // Creates the API URL to retrieve the questions from
   generateApiUrl() : void {
     this.generateApiToken();
     this.apiUrl = `https://opentdb.com/api.php?token=${this.apiToken}&amount=20&encode=url3986&category=${this.Category}&difficulty=0&type=0`;
   }
 
+  // Get the API token to prevent duplicate questions
   generateApiToken() : void {
     if (this.apiToken == "") 
     {
@@ -89,22 +91,25 @@ export class QuizApiServiceService {
     }
   }
 
+  // Get the scores from the Database
   getScores() : void {
-    this.scores=[];
+    var scorecounter = 0;
+    this.scores = [];
     this.http.get("https://mighty-lowlands-31094.herokuapp.com/scores")
     .subscribe((data : any) => {
-      for (let i = 0; i < 10; i++) {
+      for (let i = 0; i < data.length; i++) {
         if (data[i].category_id == this.Category)
         {
           let s = {
             name : decodeURIComponent(data[i].name),
             score : data[i].score,
           }
-          this.scores.push(s);
+          if (scorecounter <= 10) {
+            this.scores.push(s);
+            scorecounter++;
+          }
         }
       }
     })
   }
 }
-
-
